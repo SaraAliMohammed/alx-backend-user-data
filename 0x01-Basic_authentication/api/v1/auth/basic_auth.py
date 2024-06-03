c06f2e6795e4b6f051ac5f9fdd77cea0d8dfaa32
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-06-03 03:30:01 -0700

    Task7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,3 +14,13 @@ class BasicAuth(Auth):
     """
     class BasicAuth
     """
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+         returns the Base64 part of the Authorization
+         header for a Basic Authentication
+        """
+        if authorization_header and isinstance(
+                authorization_header,
+                str) and authorization_header.startswith("Basic "):
+            return authorization_header[6:]
